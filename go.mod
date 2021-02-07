8458a0a89374420747bde59d5b6b52df1921e359
filepang <filepang@gmail.com>
2021-02-07 16:09:05 +0900

    feature: add log, router pkg

--- go.mod
+++ go.mod
@@ -4,15 +4,14 @@ go 1.15
 
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
-	github.com/DataDog/datadog-go v4.3.1+incompatible // indirect
+	github.com/HatsuneMiku3939/ocecho v0.3.2
 	github.com/Masterminds/squirrel v1.5.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-playground/validator/v10 v10.4.1
-	github.com/go-sql-driver/mysql v1.5.0
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	go.opencensus.io v0.22.6
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/DataDog/dd-trace-go.v1 v1.28.0
 )
---
+++ pkg/router/logger.go
@@ -0,0 +1,54 @@
+package router
+
+import (
+	"time"
+
+	"github.com/org39/webapp-tutorial-backend/pkg/log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func loggerMiddleware(logger log.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			start := time.Now()
+
+			err := next(c)
+			if err != nil {
+				c.Error(err)
+			}
+
+			req := c.Request()
+			res := c.Response()
+			ctx := req.Context()
+
+			duration := time.Since(start)
+			status := res.Status
+
+			traceLogger := logger.LoggerWithSpan(ctx)
+			accessLog := traceLogger.WithFields(logrus.Fields{
+				"http_status":        status,
+				"http_host":          req.Host,
+				"http_latency":       float64(duration) / float64(1e6),
+				"http_latency_human": duration.String(),
+				"http_method":        req.Method,
+				"http_uri":           req.RequestURI,
+				"http_remote_ip":     c.RealIP(),
+			})
+
+			switch {
+			case status >= 500:
+				accessLog.Error()
+			case status >= 400:
+				accessLog.Warn()
+			case status >= 300:
+				accessLog.Debug()
+			default:
+				accessLog.Debug()
+			}
+
+			return nil
+		}
+	}
+}
